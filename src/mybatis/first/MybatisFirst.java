7186e443c8822f6a0c67a12eff16d51582062ea5
LIU <LIU@LIU-PC>
2017-05-30 00:26:11 +0800

    used as src files.

---
+++ src/mybatis/first/MybatisFirst.java
@@ -0,0 +1,43 @@
+package mybatis.first;
+
+import java.io.InputStream;
+
+import mybatis.po.User;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+public class MybatisFirst {
+
+	@Test
+	public void findUserById() {
+		
+		String resource = "/SqlMapConfig.xml";
+		InputStream inputStream = null;
+		try {
+			inputStream = Resources
+					.getResourceAsStream(resource);
+		
+			// sessionFactory
+			SqlSessionFactory sqlsessionfactory = new SqlSessionFactoryBuilder()
+					.build(inputStream);
+		
+			// sqlSession
+			SqlSession sqlsession = sqlsessionfactory.openSession();
+			
+			// db
+			User user = sqlsession.selectOne("test.findUserById",1);
+			System.out.println(user);
+			
+			//ÊÍ·Å×ÊÔ´
+			sqlsession.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
