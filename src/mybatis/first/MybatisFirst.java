1a268c2bec10b4c0626bef747ba113530eeb8cf6
liu <g17600907206@gmail.com>
2017-08-02 00:00:01 +0800

    2017-08-02

--- src/mybatis/first/MybatisFirst.java
+++ src/mybatis/first/MybatisFirst.java
@@ -7,18 +7,18 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
-import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
 
 public class MybatisFirst {
 
 	@Test
 	public void findUserById() {
 
-		String resource = "config/SqlMapConfig.xml";
+		String resource = "SqlMapConfig.xml";
 		InputStream inputStream = null;
-		File file = new File(resource);
-		System.out.println(file.exists());
 		try {
 			inputStream = Resources
 					.getResourceAsStream(resource);
@@ -31,14 +31,74 @@ public class MybatisFirst {
 			SqlSession sqlsession = sqlsessionfactory.openSession();
 			
 			// db
-			User user = sqlsession.selectOne("test.findUserById",3);
+			User user = sqlsession.selectOne("test.findUserById",1);
 			System.out.println(user.toString());
 			
 			sqlsession.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}finally {
 
-	}
+        }
+    }
+
+	@Test
+    public void findUserByName() throws IOException {
+	    String resource = "SqlMapConfig.xml";
+	    InputStream inputStream = Resources.getResourceAsStream(resource);
+	    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	    SqlSession sqlSession = sqlSessionFactory.openSession();
+	    List<User> usrlt = sqlSession.selectList("test.findUserByName","xiao");
+        System.out.println(usrlt.toString());
+        sqlSession.close();
+    }
+
+    @Test
+    public void insertUserTest() throws IOException {
+        String resource = "SqlMapConfig.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        User user = new User();
+        user.setUsername("xiaoqin1");
+        user.setSex("男");
+        user.setBirthday(new Date());
+        user.setAddress("qinhuangdao");
+        sqlSession.insert("test.insertUser",user);
+        System.out.println(user.getId());
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteUserTest() throws IOException {
+        String resource = "SqlMapConfig.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        sqlSession.delete("test.deleteUser",1);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void updateUserTest() throws IOException {
+        String resource = "SqlMapConfig.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        User user = new User();
+        user.setUsername("xiaqiang");
+        user.setId(2);
+        sqlSession.update("test.updateUser",user);
+        sqlSession.commit();
+        sqlSession.close();
+    }
 }
